b28d9c1179cd48cebe337c9e52fb97a8fd9c9b53
tv-dem <69cobak@mail.ru>
2020-03-16 23:53:28 +0300

    task complit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr1 = [];
+  let arr2 = [];
+  bracketsConfig.forEach(el => {
+    arr1.push(el[0]);
+    arr2.push(el[1]);
+  });
+  let stack = [];
+  str = str.split("");
+  for (let el of str) {
+    if (arr1.indexOf(el) == arr2.indexOf(el) && el == stack[stack.length - 1]) stack.pop();
+    else
+      if (arr1.indexOf(el) != -1) stack.push(el);
+      else {
+        let r = stack.pop();
+        if ((arr1.indexOf(r) != arr2.indexOf(el)) || arr1.indexOf(r) == -1)
+          return false;
+      }
+  }
+  return !stack.length;
 }
